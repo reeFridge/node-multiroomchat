a785db4f634015871643fb79f54974348d073476
reeFridge <reefridgerator@gmail.com>
2015-12-25 19:00:45 +0000

    Deploy

---
+++ server.js
@@ -0,0 +1,55 @@
+var http  = require('http');
+var mime = require('mime');
+var fs = require('fs');
+var path = require('path');
+var chatServer = require('./lib/chat_server');
+var cache = {};
+
+function send404(response) {
+	response.writeHead(404,{'Content-Type':'text/plain'});
+	response.write('Error 404: resource not found.');
+	response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+	response.writeHead(200,{'Content-Type':mime.lookup(path.basename(filePath))});
+	response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+	if (cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
+	} else {
+		fs.exists(absPath, function (exists) {
+			if (exists) {
+				fs.readFile(absPath, function (err, data) {
+					if (err) {
+						send404(response);
+					} else {
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			} else {
+				send404(response);
+			}
+		});
+	}
+}
+
+var server = http.createServer(function (request, response) {
+	var filePath = false;
+	if (request.url == '/') {
+		filePath = 'public/index.html';
+	} else {
+		filePath = 'public' + request.url;
+	}
+	var absPath = './' + filePath;
+	serveStatic(response, cache, absPath);
+});
+var port = process.env.PORT || 3000;
+server.listen(port, function () {
+	console.log('Server listening on port: ' + port);
+});
+
+chatServer.listen(server);
